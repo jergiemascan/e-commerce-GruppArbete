896fbea4f554f89b717cf6f84b5291294d5f0ed6
Jergie Mascan <jiji.mascan@gmail.com>
2022-03-23 10:17:27 +0100

    landing-page

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import "./index.css";
 
 function App() {
   return (
--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { IoCartOutline } from "react-icons/io5";
+import SportMan from "../bilder/sports-wear-store-banner-img-1.jpg";
+import SportWoman from "../bilder/sports-wear-store-banner-img-2.jpg";
+import Footer from "./Footer";
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Homepage</h1>
-      <h1>Product lists here</h1>
+      <main className="main">
+        <div className="left">
+          <img className="" src={SportMan} alt="man in gym" />
+          <img className="" src={SportWoman} alt="woman in gym" />
+        </div>
+        <div className="right">
+          <h1>LET'S LEVEL UP YOUR GAME</h1>
+          <h2>Fitness Ready</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br></br>
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+          <button className="btn">
+            <IoCartOutline className="btn-cart" /> SHOP NOW
+          </button>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
