31d2a0d760f872cbfe1a2b55eaf74ab5a25678ed
Rimon Hanna <hannarimon1@gmail.com>
2022-03-30 17:58:38 +0200

    First draft of Styling
    Co-authored-by: David Komar <komiie@users.noreply.github.com>

--- client-side/src/components/Product.jsx
+++ client-side/src/components/Product.jsx
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 import "./Product.css";
 
 function Product() {
   let { id } = useParams();
   const [product, setProduct] = useState([]);
-  const [update, setUpdate] = useState();
   const fetchProduct = async () => {
     try {
       const response = await fetch(`http://localhost:3001/products/${id}`);
-      let productData = await response.json();
-      setProduct((product) => [...product, productData]);
-      console.log(productData);
-      console.log(product);
+      let product = await response.json();
+      setProduct(product);
     } catch (err) {
       console.log(err);
     }
@@ -26,20 +24,26 @@ function Product() {
   return (
     <div>
       <Navbar />
-      {product.map((product) => {
-        return (
-          <div className="product-container" key={product._id}>
-            <img src={`../assets/${product.photo}`} alt="Supplement" />
-            <div>
-              <h1>{product.name}</h1>
-              <h2>{product.price}:-</h2>
-              <h3 className="product-desc">{product.desc}</h3>
-              <button type="submit">Add to cart</button>
-              <Link to={"/products"}>Product list</Link>
-            </div>
-          </div>
-        );
-      })}
+
+      <div className="product-container" key={product._id}>
+        <img
+          className="product-image"
+          src={`../assets/${product.photo}`}
+          alt="Supplement"
+        />
+        <div className="product-card">
+          <h1>{product.name}</h1>
+          <h2>&#36;{product.price}</h2>
+          <p className="product-desc">{product.desc}</p>
+          <button className=" btn-product " type="submit">
+            Add to cart
+          </button>
+          <Link className="product-link-btn" to={"/products"}>
+            Back to products..
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
