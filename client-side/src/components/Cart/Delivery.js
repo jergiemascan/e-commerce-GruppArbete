b9bcfed586a74bff9ad1c13d8394d76f2b27553b
John Landtblom <john.landtblom@live.se>
2022-03-30 11:06:08 +0200

    Commit

--- client-side/src/components/Cart/Delivery.js
+++ client-side/src/components/Cart/Delivery.js
@@ -2,34 +2,39 @@ import React, { useState } from "react";
 import { FaShippingFast } from "react-icons/fa";
 import { BsTruck } from "react-icons/bs";
 import { GoLocation } from "react-icons/go";
+import DeliveryCard from "../Cart/UI/DeliveryCard";
 
 function Delivery() {
-  const [state, setState] = useState(false);
+  const [active, setActive] = useState(false);
 
-  const changeBorderColor = () => {
-    setState(!state);
+  const handleClick = () => {
+    setActive(!active);
+    console.log(setActive);
   };
+
   return (
     <div className="delivery-container">
       <div
-        onClick={changeBorderColor}
-        className={"choose-delivery " + (state ? "choosen-delivery" : "")}
+        onClick={handleClick}
+        className={"choose-delivery " + (active ? "choosen-delivery" : "")}
       >
         <h1>Pickup Location</h1>
         <GoLocation color="blue" fontSize="4em"></GoLocation>
         <h1>FREE</h1>
       </div>
+
       <div
-        onClick={changeBorderColor}
-        className={"choose-delivery " + (state ? "choosen-delivery" : "")}
+        onClick={handleClick}
+        className={"choose-delivery " + (active ? "choosen-delivery" : "")}
       >
         <h1>Standard 3 - 4 Days</h1>
         <BsTruck color="blue" fontSize="4em"></BsTruck>
         <h1>4.99 Eur</h1>
       </div>
+
       <div
-        onClick={changeBorderColor}
-        className={"choose-delivery " + (state ? "choosen-delivery" : "")}
+        onClick={handleClick}
+        className={"choose-delivery " + (active ? "choosen-delivery" : "")}
       >
         <h1>Express 1 - 2 Days</h1>
         <FaShippingFast color="blue" fontSize="4em"></FaShippingFast>
