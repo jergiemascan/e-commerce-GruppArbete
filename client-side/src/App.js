37bf84a66ea450996a1481138e80d91c04f6b466
Jergie Mascan <jiji.mascan@gmail.com>
2022-04-03 18:41:18 +0200

    added stuff in homepage + responsiveness

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Product from "./components/Product";
@@ -10,7 +8,6 @@ import OrderConfirmation from "./components/Cart/OrderConfirmation";
 import Payment from "./components/Cart/Payment";
 import "./index.css";
 import ErrorPage from "./components/ErrorPage";
-import Profile from "./components/User/Profile";
 
 function App() {
   return (
--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -3,6 +3,8 @@ import Navbar from "./Navbar";
 import { IoCartOutline } from "react-icons/io5";
 import SportMan from "../bilder/sports-wear-store-banner-img-1.jpg";
 import SportWoman from "../bilder/sports-wear-store-banner-img-2.jpg";
+import HeartIcon from "../bilder/icon-1.png";
+import thoraxIcon from "../bilder/icon-4.png";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import History from "./User/History";
@@ -26,9 +28,9 @@ const Home = () => {
           <img className="bild1" src={SportWoman} alt="woman in gym" />
         </div>
         <div className="right">
-          <h1>LET'S LEVEL UP YOUR GAME</h1>
-          <h2>Fitness Ready</h2>
-          <p>
+          <h1 className="h1-home">LET'S LEVEL UP YOUR GAME</h1>
+          <h2 className="h2-home">Fitness Ready</h2>
+          <p className="home-p">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br></br>
             Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
@@ -37,6 +39,27 @@ const Home = () => {
           </Link>
         </div>
       </main>
+      <div className="explore-collection">
+        {/* <h2 className="h2-collection">EXPLORE OUR EXCLUSIVE PRODUCTS</h2> */}
+        <div className="offer">
+          <div className="home-div">
+            <img src={HeartIcon} alt="heart icon" className="img-home"></img>
+            <h3 className="h3-home"> HEALTH SUPPLIMENTS</h3>
+            <p className="span-home">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudianda.
+            </p>
+          </div>
+          <div className="home-div">
+            <img src={thoraxIcon} alt="body icon" className="img-home"></img>
+            <h3 className="h3-home"> OUR PLANS</h3>
+            <p className="span-home">
+              Veritatis obcaecati tenetur iure eius earum ut molestias
+              architecto voluptate aliquam nihil, eveniet aliquid.
+            </p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
