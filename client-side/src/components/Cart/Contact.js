096213fafc5fb0823f7c8b9279d8dd9d46870e06
John Landtblom <john.landtblom@live.se>
2022-04-04 21:58:29 +0200

    contact form

--- client-side/src/components/Cart/Contact.js
+++ client-side/src/components/Cart/Contact.js
@@ -1,56 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Contact({ inputData, setInputData }) {
+function Contact() {
+  const [inputData, setInputData] = useState({
+    email: "",
+    firstname: "",
+    lastname: "",
+    street: "",
+    zipcode: "",
+    city: "",
+  });
   return (
-    <div className="contact-container">
-      <input
-        type="text"
-        placeholder="Email"
-        value={inputData.email}
-        onChange={(event) =>
-          setInputData({ ...inputData, email: event.target.value })
-        }
-      ></input>
-      <input
-        type="text"
-        placeholder="Firstname"
-        value={inputData.firstname}
-        onChange={(event) =>
-          setInputData({ ...inputData, firstname: event.target.value })
-        }
-      ></input>
-      <input
-        type="text"
-        placeholder="Lastname"
-        value={inputData.lastname}
-        onChange={(event) =>
-          setInputData({ ...inputData, lastname: event.target.value })
-        }
-      ></input>
-      <input
-        type="text"
-        placeholder="Street"
-        value={inputData.street}
-        onChange={(event) =>
-          setInputData({ ...inputData, street: event.target.value })
-        }
-      ></input>
-      <input
-        type="number"
-        placeholder="ZIP Code"
-        value={inputData.zipcode}
-        onChange={(event) =>
-          setInputData({ ...inputData, zipcode: event.target.value })
-        }
-      ></input>
-      <input
-        type="text"
-        placeholder="City"
-        value={inputData.city}
-        onChange={(event) =>
-          setInputData({ ...inputData, city: event.target.value })
-        }
-      ></input>
+    <div className="reg-login">
+      <div className="contact-container">
+        <input
+          type="text"
+          value={localStorage.fullName}
+          onChange={(event) =>
+            setInputData({ ...inputData, firstname: event.target.value })
+          }
+        ></input>
+
+        <input
+          type="text"
+          placeholder="Street"
+          value={inputData.street}
+          onChange={(event) =>
+            setInputData({ ...inputData, street: event.target.value })
+          }
+        ></input>
+
+        <input
+          type="text"
+          placeholder="City"
+          value={inputData.city}
+          onChange={(event) =>
+            setInputData({ ...inputData, city: event.target.value })
+          }
+        ></input>
+        <input
+          type="number"
+          placeholder="ZIP Code"
+          value={inputData.zipcode}
+          onChange={(event) =>
+            setInputData({ ...inputData, zipcode: event.target.value })
+          }
+        ></input>
+      </div>
     </div>
   );
 }
